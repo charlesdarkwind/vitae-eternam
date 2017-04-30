c57d52a8f73942103c86c5aa3a3d5e6669cc7960
Jasmin Parent <guildwarsguy12@gmail.com>
2017-04-30 14:47:43 -0400

    hiding key

--- src/base.js
+++ src/base.js
@@ -1,7 +1,7 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-	apiKey: "AIzaSyC_RKIZsfpOVouSET_w8ioCEa6bSqq9xAs",
+	apiKey: "",
    authDomain: "urnes-fb33b.firebaseapp.com",
    databaseURL: "https://urnes-fb33b.firebaseio.com"
 });
