24cc5f0d8a356731065fb9ae50e2410be5ad2370
Jasmin Parent <guildwarsguy12@gmail.com>
2017-05-06 19:39:37 -0400

    Delete base.js

--- src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-	apiKey: "",
-   authDomain: "urnes-fb33b.firebaseapp.com",
-   databaseURL: "https://urnes-fb33b.firebaseio.com"
-});
-
-export default base;
